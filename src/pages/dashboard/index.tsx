560e9346e4b080c36304f721f56228581ad68916
AureliusAi <markus.aurelius.ai@gmail.com>
2022-02-16 10:04:27 +0900

    made some tweaks to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,8 +6,7 @@ import PageHeader from "../../page-components/PageHeader";
 import Plotly, { PlotType } from "plotly.js";
 import { initiateEchoSocket, disconnectEchoSocket, subscribeToEchoedMessage, sendEchoMessage } from "../../page-components/echo_websocket";
 import useWebSocket, { ReadyState } from "react-use-websocket";
-
-var revno: number = 0;
+import { unix_to_datetime } from "../../utils/datetime";
 
 const binance_socketUrl = "wss://stream.binance.com:9443/ws";
 
@@ -17,15 +16,17 @@ export default function Dashboard() {
   const [echoedBackText, setEchoedBackText] = useState<string>("");
   const [btcTickerData, setBtcTickerData] = useState(null);
 
+  const [tickerType1Subscribed, setTickerType1Subscribed] = useState<boolean>(false);
   const [tickerToStreamFromBinance1, setTickerToStreamFromBinance1] = useState<string | null>("btcusdt");
   const [dataTypeToStreamFromBinance1, setDataTypeToStreamFromBinance1] = useState<string | null>("trade");
   const [dataFromDT1, setDataFromDT1] = useState<string | null>("");
 
+  const [tickerType2Subscribed, setTickerType2Subscribed] = useState<boolean>(false);
   const [tickerToStreamFromBinance2, setTickerToStreamFromBinance2] = useState<string | null>("btcusdt");
   const [dataTypeToStreamFromBinance2, setDataTypeToStreamFromBinance2] = useState<string | null>("kline_1m");
   const [dataFromDT2, setDataFromDT2] = useState<string | null>("");
 
-  const [numbTotalSubscriptions, setNumbTotalSubscriptions] = useState<number | null>(null);
+  const [subscriptions, setSubscriptions] = useState<string | null>(null);
   const [bids, setBids] = useState([0]);
   const [rwData, setRWData] = useState({
     x: [] as Date[],
@@ -48,9 +49,72 @@ export default function Dashboard() {
       if ("e" in lastJsonMessage) {
         const updateType: string = lastJsonMessage["e"];
         if (updateType === "trade") {
-          setDataFromDT1(JSON.stringify(lastJsonMessage));
+          // {
+          //   "e": "trade",     // Event type
+          //   "E": 123456789,   // Event time
+          //   "s": "BNBBTC",    // Symbol
+          //   "t": 12345,       // Trade ID
+          //   "p": "0.001",     // Price
+          //   "q": "100",       // Quantity
+          //   "b": 88,          // Buyer order ID
+          //   "a": 50,          // Seller order ID
+          //   "T": 123456785,   // Trade time
+          //   "m": true,        // Is the buyer the market maker?
+          //   "M": true         // Ignore
+          // }
+          const print_msg = `[${unix_to_datetime(lastJsonMessage["T"])}] Ticker: ${lastJsonMessage["s"]} @ ${lastJsonMessage["p"]}, Qty: ${
+            lastJsonMessage["q"]
+          } (is Buyer MM? ${lastJsonMessage["m"]})`;
+
+          setDataFromDT1(print_msg);
         } else if (updateType == "kline") {
-          setDataFromDT2(JSON.stringify(lastJsonMessage));
+          // {
+          //   "e": "kline",     // Event type
+          //   "E": 123456789,   // Event time
+          //   "s": "BNBBTC",    // Symbol
+          //   "k": {
+          //     "t": 123400000, // Kline start time
+          //     "T": 123460000, // Kline close time
+          //     "s": "BNBBTC",  // Symbol
+          //     "i": "1m",      // Interval
+          //     "f": 100,       // First trade ID
+          //     "L": 200,       // Last trade ID
+          //     "o": "0.0010",  // Open price
+          //     "c": "0.0020",  // Close price
+          //     "h": "0.0025",  // High price
+          //     "l": "0.0015",  // Low price
+          //     "v": "1000",    // Base asset volume
+          //     "n": 100,       // Number of trades
+          //     "x": false,     // Is this kline closed?
+          //     "q": "1.0000",  // Quote asset volume
+          //     "V": "500",     // Taker buy base asset volume
+          //     "Q": "0.500",   // Taker buy quote asset volume
+          //     "B": "123456"   // Ignore
+          //   }
+          // }
+          const kObj = lastJsonMessage["k"];
+          const print_msg = `[${unix_to_datetime(kObj["T"])}] Ticker: ${kObj["s"]} @ ${kObj["i"]}, O: ${kObj["o"]}, H: ${kObj["h"]}, L: ${kObj["l"]}, C: ${
+            kObj["c"]
+          }. num trades: ${kObj["n"]}, base asset vol: ${kObj["v"]}`;
+
+          setDataFromDT2(print_msg);
+        }
+      } else {
+        // should be a subscriptions check call
+        if ("result" in lastJsonMessage) {
+          if (lastJsonMessage["result"] === null) {
+            setSubscriptions(null);
+          } else {
+            let res = lastJsonMessage["result"];
+            var res_str = "";
+            if (res instanceof Array) {
+              for (var i = 0; i < res.length; i++) {
+                res_str += i + 1 + ": " + res[i] + " ";
+              }
+            }
+            console.log(res_str);
+            setSubscriptions(res_str);
+          }
         }
       }
     }
@@ -67,48 +131,44 @@ export default function Dashboard() {
   }[readyState];
 
   // this is how you start and stop feeds from binance API
-  const handleSubscribeDataType1 = useCallback(
-    () =>
-      sendJsonMessage({
-        method: "SUBSCRIBE",
-        params: [`${tickerToStreamFromBinance1}@${dataTypeToStreamFromBinance1}`],
-        id: 1,
-      }),
-    [sendJsonMessage]
-  );
+  const handleSubscribeDataType1 = useCallback(() => {
+    sendJsonMessage({
+      method: "SUBSCRIBE",
+      params: [`${tickerToStreamFromBinance1}@${dataTypeToStreamFromBinance1}`],
+      id: 1,
+    });
+    setTickerType1Subscribed(true);
+  }, [sendJsonMessage]);
 
-  const handleUnSubscribeDataType1 = useCallback(
-    () =>
-      sendJsonMessage({
-        method: "UNSUBSCRIBE",
-        params: [`${tickerToStreamFromBinance1}@${dataTypeToStreamFromBinance1}`],
-        id: 1,
-      }),
-    [sendJsonMessage]
-  );
+  const handleUnSubscribeDataType1 = useCallback(() => {
+    sendJsonMessage({
+      method: "UNSUBSCRIBE",
+      params: [`${tickerToStreamFromBinance1}@${dataTypeToStreamFromBinance1}`],
+      id: 1,
+    });
+    setTickerType1Subscribed(false);
+  }, [sendJsonMessage]);
 
   //---------------------------------------------------------------------------
 
   // this is how you start and stop feeds from binance API
-  const handleSubscribeDataType2 = useCallback(
-    () =>
-      sendJsonMessage({
-        method: "SUBSCRIBE",
-        params: [`${tickerToStreamFromBinance2}@${dataTypeToStreamFromBinance2}`],
-        id: 2,
-      }),
-    [sendJsonMessage]
-  );
+  const handleSubscribeDataType2 = useCallback(() => {
+    sendJsonMessage({
+      method: "SUBSCRIBE",
+      params: [`${tickerToStreamFromBinance2}@${dataTypeToStreamFromBinance2}`],
+      id: 2,
+    });
+    setTickerType2Subscribed(true);
+  }, [sendJsonMessage]);
 
-  const handleUnSubscribeDataType2 = useCallback(
-    () =>
-      sendJsonMessage({
-        method: "UNSUBSCRIBE",
-        params: [`${tickerToStreamFromBinance2}@${dataTypeToStreamFromBinance2}`],
-        id: 2,
-      }),
-    [sendJsonMessage]
-  );
+  const handleUnSubscribeDataType2 = useCallback(() => {
+    sendJsonMessage({
+      method: "UNSUBSCRIBE",
+      params: [`${tickerToStreamFromBinance2}@${dataTypeToStreamFromBinance2}`],
+      id: 2,
+    });
+    setTickerType2Subscribed(false);
+  }, [sendJsonMessage]);
 
   const handleClickGetSubscriptions = useCallback(
     () =>
@@ -154,8 +214,8 @@ export default function Dashboard() {
               <Box mx={1}> | </Box>
               <Box style={{ fontWeight: "bold", color: "#35a660" }}>{connectionStatus}</Box>
             </Box>
-            <Box display="flex">
-              <Box>{numbTotalSubscriptions && numbTotalSubscriptions}</Box>
+            <Box sx={{ alignItems: "center" }} display="flex">
+              <Box>{subscriptions && subscriptions}</Box>
               <Button variant="outlined" sx={{ ml: 1 }} onClick={handleClickGetSubscriptions}>
                 Check Subscriptions
               </Button>
@@ -163,10 +223,20 @@ export default function Dashboard() {
           </Box>
           <Box display="flex" sx={{ mb: 1, alignItems: "center" }}>
             <Box sx={{ mr: 2, color: "#999999" }}>1. </Box>
-            <Button variant="outlined" sx={{ marginRight: 1 }} onClick={handleSubscribeDataType1} disabled={readyState !== ReadyState.OPEN}>
+            <Button
+              variant="outlined"
+              sx={{ marginRight: 1 }}
+              onClick={handleSubscribeDataType1}
+              disabled={readyState !== ReadyState.OPEN || tickerType1Subscribed}
+            >
               Subscribe
             </Button>
-            <Button variant="outlined" sx={{ marginRight: 2 }} onClick={handleUnSubscribeDataType1} disabled={readyState !== ReadyState.OPEN}>
+            <Button
+              variant="outlined"
+              sx={{ marginRight: 2 }}
+              onClick={handleUnSubscribeDataType1}
+              disabled={readyState !== ReadyState.OPEN || !tickerType1Subscribed}
+            >
               Unsubscribe
             </Button>
             <Box>data type </Box>
@@ -179,10 +249,20 @@ export default function Dashboard() {
           </Box>
           <Box display="flex" sx={{ alignItems: "center" }}>
             <Box sx={{ mr: 2, color: "#999999" }}>2. </Box>
-            <Button variant="outlined" sx={{ marginRight: 1 }} onClick={handleSubscribeDataType2} disabled={readyState !== ReadyState.OPEN}>
+            <Button
+              variant="outlined"
+              sx={{ marginRight: 1 }}
+              onClick={handleSubscribeDataType2}
+              disabled={readyState !== ReadyState.OPEN || tickerType2Subscribed}
+            >
               Subscribe
             </Button>
-            <Button variant="outlined" sx={{ marginRight: 2 }} onClick={handleUnSubscribeDataType2} disabled={readyState !== ReadyState.OPEN}>
+            <Button
+              variant="outlined"
+              sx={{ marginRight: 2 }}
+              onClick={handleUnSubscribeDataType2}
+              disabled={readyState !== ReadyState.OPEN || !tickerType2Subscribed}
+            >
               Unsubscribe
             </Button>
             <Box>data type </Box>
